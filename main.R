49df6e46679275a07aa566c1584f7f812bfde03d
chararchter <lentenis@inbox.lv>
2019-02-05 17:40:55 +0200

    Reading .csv files

--- main.R
+++ main.R
@@ -1,4 +1,21 @@
 library(ggplot2)
 
-setwd("F:\\Users\\Janis\\VIKA\\code\\")
+setwd("F:\\Users\\Janis\\VIKA\\data\\")
+lstData = list.files(pattern="*.csv")
 
+year = 2019
+month = formatC(seq(1, 12), width=2, flag=0)
+day = formatC(seq(1, 31), width=2, flag=0)
+
+csvInput = function(i){
+  filename = read.csv(lstData[i], header = TRUE, sep = ",")
+  return(filename)
+}
+
+for (i in length(lstData)){
+  filename = csvInput(i)
+  kwh = grep("kwh", lstData, value = TRUE)
+  # if (is.empty(grep("kwh", lstData, value = TRUE)) == FALSE){
+  #   print(head(filename))
+  # }
+}
