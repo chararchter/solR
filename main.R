f8ca57f87503bb89dbd641877e492389c550065f
chararchter <lentenis@inbox.lv>
2019-02-05 18:03:50 +0200

    datKWH with formated column names and plotable

--- main.R
+++ main.R
@@ -7,15 +7,9 @@ year = 2019
 month = formatC(seq(1, 12), width=2, flag=0)
 day = formatC(seq(1, 31), width=2, flag=0)
 
-csvInput = function(i){
-  filename = read.csv(lstData[i], header = TRUE, sep = ",")
-  return(filename)
-}
+colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
+datKWH = read.csv(grep("kwh", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")
 
-for (i in length(lstData)){
-  filename = csvInput(i)
-  kwh = grep("kwh", lstData, value = TRUE)
-  # if (is.empty(grep("kwh", lstData, value = TRUE)) == FALSE){
-  #   print(head(filename))
-  # }
-}
+dat = read.csv(grep("main", lstData, value = TRUE), header = TRUE, sep = ",")
+
+print(head(datKWH))
