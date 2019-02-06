218c0febaea6168cc67e4eaeb5beda5270ac3197
chararchter <lentenis@inbox.lv>
2019-02-06 17:22:14 +0200

    First graph done

--- main.R
+++ main.R
@@ -1,3 +1,4 @@
+# library(tidyverse)
 library(ggplot2)
 
 setwd("F:\\Users\\Janis\\VIKA\\data\\")
@@ -7,15 +8,22 @@ lstData = list.files(pattern="*.csv")
 # month = formatC(seq(1, 12), width=2, flag=0)
 # day = formatC(seq(1, 31), width=2, flag=0)
 
-dat = read.csv(grep("main", lstData, value = TRUE), header = TRUE, sep = ",")
+# dat = read.csv(grep("main", lstData, value = TRUE), header = TRUE, sep = ",")
 
 colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
+datKWH = read.csv(grep("kwh", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")
 
 # convert timestamp class from factor to POSIXct
 datKWH$timestamp <- as.POSIXct(strptime(datKWH$timestamp, format="%Y-%m-%d %H:%M:%S"))
 
-# check if timestamp is still readable
 print(head(datKWH))
 
-# check classes of the columns
-print(sapply(datKWH, class))
+g = ggplot(datKWH, aes(x = datKWH[, 'timestamp'], y = datKWH[, 'gridToBattery'])) + geom_point()
+g = g + xlab("Time") + ylab("Grid To Battery, kWh")
+# scale_*_datetime for datetimes (class POSIXct),
+g = g + scale_x_datetime(breaks='1 week', labels = "%W")
+
+# use tidyverse to get rid of NA values
+# datKWH %>%
+#   filter(!is.na(datKWH[, 'gridToBattery'])) %>%
+#   ggplot(datKWH, aes(x = datKWH[, 'timestamp'], y = datKWH[, 'gridToBattery'])) + geom_point()
