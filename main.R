deef9e25f1359aec98d48b4e25977fe6db44ea9e
chararchter <lentenis@inbox.lv>
2019-02-06 15:12:46 +0200

    Changed timestamp class from factor to POSIXct

--- main.R
+++ main.R
@@ -3,13 +3,19 @@ library(ggplot2)
 setwd("F:\\Users\\Janis\\VIKA\\data\\")
 lstData = list.files(pattern="*.csv")
 
-year = 2019
-month = formatC(seq(1, 12), width=2, flag=0)
-day = formatC(seq(1, 31), width=2, flag=0)
-
-colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
-datKWH = read.csv(grep("kwh", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")
+# year = 2019
+# month = formatC(seq(1, 12), width=2, flag=0)
+# day = formatC(seq(1, 31), width=2, flag=0)
 
 dat = read.csv(grep("main", lstData, value = TRUE), header = TRUE, sep = ",")
 
+colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
+
+# convert timestamp class from factor to POSIXct
+datKWH$timestamp <- as.POSIXct(strptime(datKWH$timestamp, format="%Y-%m-%d %H:%M:%S"))
+
+# check if timestamp is still readable
 print(head(datKWH))
+
+# check classes of the columns
+print(sapply(datKWH, class))
