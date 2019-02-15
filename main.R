8c757b0d23e46218cae0c03f391cadfb37529994
chararchter <lentenis@inbox.lv>
2019-02-15 16:18:35 +0200

    Wrote seperate functions for import and processing of data.

--- main.R
+++ main.R
@@ -3,17 +3,57 @@ library(ggplot2)
 library(lubridate)
 library(dplyr)
 
-setwd("..\\data\\")
-lstData = list.files(pattern="*.csv")
+meteoData = function(){
+    setwd("F:\\Users\\Janis\\VIKA\\solR\\data\\meteo\\")
+    lstData = list.files(pattern="*.csv")
+}
 
-colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
-datKWH = read.csv(grep("kwh", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")
+solarSummary = function(){
+    setwd("F:\\Users\\Janis\\VIKA\\solR\\data\\solar\\")
+    lstData = list.files(pattern="*.csv")
+    colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
+    datKWH = read.csv(grep("main", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")    
+}
 
-# convert timestamp class from factor to POSIXct
-datKWH$timestamp <- as.POSIXct(strptime(datKWH$timestamp, format="%Y-%m-%d %H:%M:%S"))
+solarData = function(){
+    setwd("F:\\Users\\Janis\\VIKA\\solR\\data\\solar\\")
+    lstData = list.files(pattern="*.csv")
+    colNamesKHW  = c("timestamp", "gridToBattery", "gridToConsumers", "PVToBattery", "PVToGrid", "PVToConsumers", "batteryToConsumers", "batteryToGrid", "gensetToConsumers", "gensetToBattery", "gas")
+    datKWH = read.csv(grep("kwh", lstData, value = TRUE), skip = 2, header = FALSE, col.names = colNamesKHW, sep = ",")    
+}
 
-# replace NA values with 0
-datKWH[is.na(datKWH)] <- 0
+
+year = 2019
+month = formatC(seq(1, 12), width=2, flag=0)
+day = formatC(seq(1, 31), width=2, flag=0)
+
+interpretFilename = function(filename){
+    # input: name of the date file
+    # output: you know which day month and year it is by parsing filename
+    # NEVERMIND THIS IS ATROCIOUS WAY TO IMPLEMENT IT CUZ
+    # CONSIDER IN 0101 THERE IS 01 AND ALSO 10
+    # TO BE DELETED
+    #define possible patterns
+    years = 2019
+    months = formatC(seq(1, 12), width=2, flag=0)
+    days = formatC(seq(1, 31), width=2, flag=0)
+    
+    year = patternSearch(filename, years)
+    month = patternSearch(filename, months)
+    day = patternSearch(filename, days)
+    return(c(year, month, day))
+}
+
+fixDatetime = function(data){
+    # convert timestamp class from factor to POSIXct
+    data$timestamp <- as.POSIXct(strptime(data$timestamp, format="%Y-%m-%d %H:%M:%S"))
+    # replace NA values with 0
+    data[is.na(data)] <- 0
+    return(data)
+}
+
+datKWH = solarData()
+datKWH = fixDatetime(datKWH)
 
 timestamp = datKWH$timestamp
 gridToBattery = datKWH$gridToBattery
@@ -23,12 +63,10 @@ sharedTheme = theme_minimal()
 # Change the appearance of the main title
 sharedTheme = sharedTheme + theme(plot.title = element_text(size=18, face="bold",margin = margin(10, 0, 10, 0)))
 sharedAxis = xlab("Time")
-# scale_*_datetime for datetimes (class POSIXct),
-# scale_x_datetime(breaks='1 week', labels = "%W")
 
 pltWeekStats = function(data, timestamp, dependentVar, nor, i){
     # Summarize gridToBattery by 2 hours in a 2 days interval
-    data %>% group_by(timestamp=floor_date(timestamp, "2 hours")) %>%
+    data %>% group_by(timestamp=floor_date(timestamp, "1 hour")) %>%
     summarize(gridToBattery=sum(gridToBattery))  %>%
     ggplot(aes(x = timestamp, y = gridToBattery)) + geom_point() + sharedTheme +
     sharedAxis + ylab("Grid To Battery, kWh") +
@@ -38,9 +76,8 @@ pltWeekStats = function(data, timestamp, dependentVar, nor, i){
 }
 
 weekStats <- function(data, timestamp, dependentVar){
-    setwd("..\\plots\\")
+    setwd("F:\\Users\\Janis\\VIKA\\solR\\plots\\")
     nor = min(timestamp)
-    print(nor)
     i = 1
     while (interval(date(nor), (date(nor) + days(2))) %within% interval(date(min(timestamp)), (date(max(timestamp))))) {
         pltWeekStats(datKWH, timestamp, gridToBattery, nor, i)
@@ -51,6 +88,7 @@ weekStats <- function(data, timestamp, dependentVar){
 
 weekStats(datKWH, timestamp, gridToBattery)
 
+# Plots for a month
 datKWH %>% group_by(timestamp=floor_date(timestamp, "2 hours")) %>%
     summarize(gridToBattery=sum(gridToBattery)) %>%
     ggplot(aes(x = timestamp, y = gridToBattery)) + geom_point() + sharedTheme + 
