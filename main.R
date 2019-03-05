e1df464bcc45d1737ce81c038cc18aba992c6ccf
chararchter <lentenis@inbox.lv>
2019-03-05 10:59:10 +0200

    Working splines on meteo data

--- main.R
+++ main.R
@@ -189,19 +189,6 @@ units = c('V', 'A', 'W')
 #     }
 # }
 
-intrval1 = c(as_datetime("2019-01-19 07:00:00 UTC"), as_datetime("2019-01-19 07:00:00 UTC") + hours(48))
-
-
-spl_Meteo1 = spline(datMeteo$timestamp, datMeteo$solarIrradiance)
-spl_Meteo2 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-2)
-
-plot.new()
-jpeg('splines2.jpeg', width = 1000, height = 600, units = "px", pointsize = 15)
-plot(datMeteo$timestamp, datMeteo$solarIrradiance, col="gray40", xlab = "Time", ylab ="Solar irradiance, W/m2")
-lines(spl_Meteo1, col = "purple", lwd = 2)
-lines(spl_Meteo2, col = "blue", lwd = 2)
-title(main = 'Meteo dati par janvāri', cex.main = 2, font.main= 4, col.main= "black")
-dev.off()
 
 # Error: Aesthetics must be either length 1 or the same as the data (133920): x, y
 # possible solution - plot spline as data series
@@ -211,13 +198,47 @@ dev.off()
 #     ggtitle("Meteo") + sharedAxis + ylab("Solar irradiance, W/m2")
 # ggsave("meteo.pdf", width = width, height = height, units = "cm")
 
+
+###########################
+#splines
+###########################
+
+intrval1 = c(as_datetime("2019-01-19 06:00:00 UTC"), as_datetime("2019-01-19 07:00:00 UTC") + hours(110))
+
+# how many entries are there in intrval1? needed to decide nknots, which determines smoothing
+intrRow = nrow(datMeteo %>%  filter(between(timestamp, min(intrval1), max(intrval1))))
+
+spl_Meteo1 = spline(datMeteo$timestamp, datMeteo$solarIrradiance)
+# spl_Meteo1 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-2, all.knots = TRUE)
+spl_Meteo2 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-3, all.knots = TRUE)
+spl_Meteo3 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-3, nknots = floor(0.45*intrRow))
+spl_Meteo4 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-3, nknots = floor(0.35*intrRow))
+# spl_Meteo4 = smooth.spline(datMeteo$timestamp, datMeteo$solarIrradiance,spar = 1e-7, tol = 1e-3, nknots = intrRow-10)
+
 spl_Meteo1 = data.frame(spl_Meteo1)
 spl_Meteo1$x <- lubridate::as_datetime(spl_Meteo1$x)
-dplyr::arrange(spl_Meteo1, x)
+spl_Meteo1 = dplyr::arrange(spl_Meteo1, x)
 
 spl_Meteo2 = data.frame(x = spl_Meteo2$x, y = spl_Meteo2$y)
 spl_Meteo2$x <- lubridate::as_datetime(spl_Meteo2$x)
-dplyr::arrange(spl_Meteo2, x)
+spl_Meteo2 = dplyr::arrange(spl_Meteo2, x)
+
+spl_Meteo3 = data.frame(x = spl_Meteo3$x, y = spl_Meteo3$y)
+spl_Meteo3$x <- lubridate::as_datetime(spl_Meteo3$x)
+spl_Meteo3 = dplyr::arrange(spl_Meteo3, x)
+
+spl_Meteo4 = data.frame(x = spl_Meteo4$x, y = spl_Meteo4$y)
+spl_Meteo4$x <- lubridate::as_datetime(spl_Meteo4$x)
+spl_Meteo4 = dplyr::arrange(spl_Meteo4, x)
+
+# cubic spline doesnt work. datetime seems at fault. fix it.
+# pp = cubicspline (datMeteo$timestamp , datMeteo$solarIrradiance )
+# ppfun = function ( xspl ) ppval (pp , xspl )
+# # xspline = seq (0 ,150 ,0.5)
+# xspl = seq.POSIXt(from=as_datetime(min(intrval1)), to=as_datetime(max(intrval1)), by="hour")
+# cubicspline (datMeteo$timestamp , datMeteo$solarIrradiance , xi = xspl )
+# yspl = cubicspline (datMeteo$timestamp , datMeteo$solarIrradiance , xspl )
+
 
 plot0 <- datMeteo %>%
     ggplot() +
@@ -235,15 +256,35 @@ plot1 <- spl_Meteo1 %>%
 
 plot2 <- spl_Meteo2 %>%
     ggplot() +
-    geom_line(aes(x = x, y = y), size = 0.5, alpha = 0.75) +
-    ylab("Smoothing spline") + coord_cartesian(xlim = intrval1) +
+    geom_line(aes(x = x, y = y), size = 0.5, alpha = 0.75) + 
+    ylab("Smoothing spline all knots") + coord_cartesian(xlim = intrval1) +
     theme_minimal() +
     theme(axis.title.x = element_blank())
 
+plot3 <- spl_Meteo3 %>%
+    ggplot() +
+    geom_line(aes(x = x, y = y), size = 0.5, alpha = 0.75) +
+    ylab("Smoothing spline nknots = 45% all knots") + coord_cartesian(xlim = intrval1) +
+    theme_minimal() +
+    theme(axis.title.x = element_blank())
+
+plot4 <- spl_Meteo4 %>%
+    ggplot() +
+    geom_line(aes(x = x, y = y), size = 0.5, alpha = 0.75) +
+    ylab("Smoothing spline nknots = 35% all knot") + coord_cartesian(xlim = intrval1) +
+    theme_minimal() +
+    theme(axis.title.x = element_blank())
+
+# plot2 <- ggplot() +
+#     geom_line(aes(x = xspl, y = yspl), size = 0.5, alpha = 0.75) +
+#     ylab("Cubic spline") + coord_cartesian(xlim = intrval1) +
+#     theme_minimal() +
+#     theme(axis.title.x = element_blank())
+
 pdf('testSplines.pdf',
     width=8,
-    height=9)
+    height=12)
 grid.arrange(
-    arrangeGrob(plot0,plot1,plot2,nrow=3,heights=c(.3,.3,.4))
+    arrangeGrob(plot0,plot2,plot3,plot4,nrow=4,heights=c(.25,.25,.25,.25))
 )
 dev.off()
