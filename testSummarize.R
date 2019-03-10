e3e307600138f9ae963b3ffab868350bd05cebe9
chararchter <30589284+chararchter@users.noreply.github.com>
2019-03-10 14:11:58 +0200

    Add files via upload
    everything works just fine here. keep digging your original example

--- testSummarize.R
+++ testSummarize.R
@@ -2,11 +2,14 @@ library(ggplot2)
 library(lubridate)
 library(dplyr)
 
-timestamp <- seq(ymd('2019-01-01'),ymd('2019-01-12'), by = '30 min')
-# solVar <- rnorm(length(timestamp))
+timestamp <- seq.POSIXt(from=as.POSIXct("2019-01-01 00:00:00"), to=as.POSIXct("2019-01-01 23:00:00"), by= '15 min')
+solVar <- rnorm(length(timestamp))
 solVar = seq(from = 1, to = 100, length.out = length(timestamp))
 data <- data.frame(timestamp, solVar)
 
+tab1 = data %>% group_by(timestamp=floor_date(timestamp, "6 hours")) %>%
+  summarize(solVar=sum(solVar))
+
 plt = data %>% group_by(timestamp=floor_date(timestamp, "6 hours")) %>%
-    summarize(solVar=sum(solVar)) %>%
-    ggplot(aes(x = timestamp, y = solVar)) + geom_point() 
+summarize(solVar=sum(solVar)) %>%
+ggplot(aes(x = timestamp, y = solVar)) + geom_point() 
