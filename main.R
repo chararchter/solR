40ba90be4c383a5eaa486258eecf8aa26f03a69b
chararchter <lentenis@inbox.lv>
2019-03-20 16:09:43 +0200

    First calculated kWh for a month

--- main.R
+++ main.R
@@ -2,9 +2,10 @@
 library(ggplot2)
 library(lubridate)
 library(dplyr)
-library(gridExtra) # combining 2 plots together in a grid
-library(gridGraphics) # cheat solution for only 'grobs' allowed in "gList" error I found on stack exchange
-library(pracma) # cubic spline
+library(DescTools) # for trapezoid integrating
+# library(gridExtra) # combining 2 plots together in a grid
+# library(gridGraphics) # cheat solution for only 'grobs' allowed in "gList" error I found on stack exchange
+# library(pracma) # cubic spline
 
 default = "F:\\Users\\Janis\\VIKA\\solR\\"
 solNames = c("solD40", "solD13","solA13", "solR13", "solD90")
@@ -64,7 +65,9 @@ pltMonth = function(solName){
     labelSolVar = paste(whichDevice(solname["device"]), whichMeasure(solname["unit"]), ", ", solname["unit"], sep = "")
     measurement = paste("_", solname["device"], solname["unit"], sep = "")
     
-    intrval1 = c(min(datSol$timestamp), min(datSol$timestamp) + days(31))
+    # intrval1 = c(min(datSol$timestamp), min(datSol$timestamp) + days(1))
+    x = min(datSol$timestamp) + days(21)
+    intrval1 = c(x, x + days(1))
     intrval2 = toString(interval(intrval1[1], intrval1[2]))
 
     timestamp = datSol$timestamp
@@ -72,16 +75,70 @@ pltMonth = function(solName){
     
     datTemp = data.frame(timestamp, solVar)
     # write.csv(datTemp, file = "datTemp.csv")
+    
+    
+    if (solname["unit"]=="W"){
+        # print(head(datTemp))
+        # areaUnderTheCurve = AUC(datTemp$timestamp, datTemp$solVar, method = "trapezoid", na.rm = FALSE)
+        # print(areaUnderTheCurve)
+        
+        x = timestamp
+        y = solVar
+        t = 0
+        s = 0
+        
+        for (i in 1:(length(x)-1)){
+            deltx = x[i+1] - x[i]
+            yvid = (y[i] + y[i+1])/2
+            s[i] = deltx * yvid
+            t = t+s[i]
+        }
+        kwHmonth = t /3600
+        
+        print("~~~~~~~~~~~~~~~~~~~~~")
+        print(paste(solName,"    ", "kWh =", kwHmonth, sep = " "))
+        print("~~~~~~~~~~~~~~~~~~~~~")
+        
+        # timestamp = datSol$timestamp
+        # nor = min(timestamp)
+        # i = 0
+        
+        # while (interval(date(nor), (date(nor) + days(7))) %within% interval(date(min(timestamp)), (date(max(timestamp))))) {
+        #     print("viens")
+        #     # print(length(interval(date(nor), (date(nor) + days(7)))))
+        #     # strt = which(nor == timestamp)[[1]]
+        #     # print(timestamp[toString(nor):toString(nor + days(7))])
+        #     # match(c(nor, nor+days(7)),timestamp)
+        #     
+        #     dd = nor + days(7)
+        #     print(date(dd))
+        #     t = 0
+        #     s = 0
+        # 
+        #     for (i in strt:endidx){
+        #         deltx = x[i+1] - x[i]
+        #         yvid = (y[i] + y[i+1])/2
+        #         s[i] = deltx * yvid
+        #         t = t+s[i]
+        #     }
+        #     print(t)
+        #     
+        #     nor =  nor + days(7)
+        #     # i = i + 1
+        # }
+        
+    }
+    
 
     smry = datTemp %>% group_by(x2=floor_date(timestamp, "1 day")) %>%
         summarize(y2=sum(solVar))
     
-    # plotSol <- datSol %>% ggplot(aes(x = timestamp, y = solVar)) + geom_point() +
-    #     sharedTheme +
-    #     coord_cartesian(xlim = intrval1) +
-    #     sharedAxis + ylab(labelSolVar) +
-    #     ggtitle(paste(panelVerbose, intrval2))
-    # ggsave(paste("sol", panel, measurement, ".pdf",sep=""), width = width, height = height, units = "cm")
+    plotSol <- datSol %>% ggplot(aes(x = timestamp, y = solVar)) + geom_line() +
+        sharedTheme +
+        coord_cartesian(xlim = intrval1) +
+        sharedAxis + ylab(labelSolVar) +
+        ggtitle(paste(panelVerbose, intrval2))
+    ggsave(paste("sol", panel, measurement, ".pdf",sep=""), width = width, height = height, units = "cm")
     
     datTemp %>% group_by(x2=floor_date(timestamp, "1 day")) %>%
         summarize(y2=sum(solVar)) %>%
@@ -95,9 +152,12 @@ pltMonth = function(solName){
         ggtitle(paste("Summary:", panelVerbose, intrval2))
     ggsave(paste("sol", panel, measurement, "sumWeek.pdf",sep=""), width = width, height = height, units = "cm")
     
-    # return(smry)
-    return(datTemp)
+    return(smry)
+    # return(datTemp)
     # return(c(datTemp, smry))
+    
+    # lst = c(solName, kWhmonth)
+    # return(lst)
 }
 
 
@@ -111,8 +171,17 @@ types = c('JA','LG')
 devices = c('_Bat', '_PV_')
 units = c('V', 'A', 'W')
 
-sumry = pltMonth("solD40JA_PV_W")
-print(sumry)
+
+kWhmonths = data.frame(solVar = 0, value = 0)
+
+# sumry = pltMonth("solD40JA_PV_W")
+smry = pltMonth("solD40JA_PV_W")
+# print(smry)
+# 
+# kWhmonths <- rbind(kWhmonth, data.frame(x = kWhMonth[1], kWhMonth[2]))
+
+
+# print(sumry)
 
 # disable as it produces 60 plots and runs slow
 # for (solName in solNames){
