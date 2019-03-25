d566a8f136106592161a5193f1e4ec9e645244c0
chararchter <lentenis@inbox.lv>
2019-03-25 17:30:43 +0200

    Removed unnecessary chunck of code

--- main.R
+++ main.R
@@ -56,8 +56,10 @@ sharedTheme = sharedTheme + theme(plot.title = element_text(size=18, face="bold"
 sharedAxis = xlab("Time")
 
 trapezoidArea = function(x, y){
-    # x = timestamp
-    # y = solVar
+    # Calculate the area under the curve formed by connecting all points by a direct line
+    # (composite trapezoid rule). The curve must be given by vectors of xy-coordinates.
+    # Output - numeric value of area under the curve.
+    
     t = 0
     s = 0
     
@@ -71,13 +73,16 @@ trapezoidArea = function(x, y){
 }
 
 integrateInterval = function(lowerLimit, delta_t, datTemp){
-    upperLimit = lowerLimit + delta_t
-    timestamp = datTemp$timestamp
+    # Defines lower and upper limits for trapezoidArea() integral.
+    # Output - data frame, where "time" - lower limit of interval;
+    # "sumkWh"  - value of area under the curve in that interval.
     
+    timestamp = datTemp$timestamp
+    upperLimit = lowerLimit + delta_t
+
     intrBig = (date(max(timestamp))-lowerLimit)
     intrSmall = (upperLimit - lowerLimit)
     itrTimes = floor(as.numeric(intrBig) / as.numeric(intrSmall))
-    print(itrTimes)
     
     x <- as_datetime(itrTimes)
     y <- numeric(itrTimes)
@@ -95,11 +100,7 @@ integrateInterval = function(lowerLimit, delta_t, datTemp){
         print(paste(interval(lowerLimit, upperLimit),"   ", "kWh =", format(kWh, digits = 2, nsmall=2), sep = " "))
 
         intLength = int_length(interval(timestamp[strtIndex], timestamp[endIndex]))
-
-        print(strtIndex)
-        print(date(datTemp$timestamp[strtIndex]))
-        x[i] <- date(datTemp$timestamp[strtIndex])
-        # x[i] <- interval(lowerLimit, upperLimit)
+        x[i] <- date(timestamp[strtIndex])
         y[i] <- kWh
         
         lowerLimit =  upperLimit
@@ -108,7 +109,7 @@ integrateInterval = function(lowerLimit, delta_t, datTemp){
     }
     print(count)
     sumInt = data.frame("time" = x, "sumkWh" = y)
-    print(sumInt)
+    return(sumInt)
 }
 
 pltMonth = function(solName){
@@ -126,45 +127,27 @@ pltMonth = function(solName){
     solVar = datSol[, colIndex]
     datTemp = data.frame(timestamp, solVar)
     
+    
     if (solname["unit"]=="W"){
         t = trapezoidArea(datTemp$timestamp, datTemp$solVar)
         kwHmonth = t /3600
         print(solName)
         print(paste(interval(date(min(timestamp)), date(max(timestamp))),"   ", "kWh =", format(kwHmonth, digits = 2, nsmall=2), sep = " "))
         
-        nor = date(min(timestamp))
-        countByDays = 0
-        i = 0
-        x <- as_datetime(4)
-        y <- numeric(4)
-
-        print("########")
-        while (interval(nor, nor + hours(24)) %within% interval(nor, date(max(timestamp)))) {
-            n2 = nor + hours(24)
-            strtIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(nor)))[1]
-            endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(n2)))[1]
-            datInt = datTemp[strtIndex:endIndex,]
-
-            t = trapezoidArea(datInt$timestamp, datInt$solVar)
-            kwHday = t / 3600
-            countByDays = countByDays + kwHday
-            print(paste(interval(nor, n2),"   ", "kWh =", format(kwHday, digits = 2, nsmall=2), sep = " "))
-
-            intLength = int_length(interval(timestamp[strtIndex], timestamp[endIndex]))
-            # kwHweek = t /intLength
-            # print(paste("Interval length in seconds", intLength))
-
-            x[i] <- datSol$timestamp[endIndex]
-            y[i] <- kwHday
-
-            nor =  nor + hours(24)
-            i = i + 1
-        }
-
-        sumDays = data.frame(x, y)
-        sumDays$x <- lubridate::as_date(sumDays$x)
-        print(countByDays)
-        print(sumDays)
+        sumDays = integrateInterval(date(min(timestamp)), days(1), datTemp)
+        sumWeeks = integrateInterval(date(min(timestamp)), days(7), datTemp)
+        
+        plotDaykWh <- sumDays %>% ggplot(aes(x = time, y = sumkWh)) + geom_point() +
+            sharedTheme +
+            sharedAxis + ylab("kWh") +
+            ggtitle(paste(panelVerbose, "kWh"))
+        ggsave(paste("sol", panel, measurement, "kwh.pdf",sep=""), width = width, height = height, units = "cm")
+        
+        plotWeekkWh <- sumWeeks %>% ggplot(aes(x = time, y = sumkWh)) + geom_point() +
+            sharedTheme +
+            sharedAxis + ylab("kWh") +
+            ggtitle(paste(panelVerbose, "kWh"))
+        ggsave(paste("sol", panel, measurement, "kwh.pdf",sep=""), width = width, height = height, units = "cm")
     }
     
     smry = datTemp %>% group_by(x2=floor_date(timestamp, "1 day")) %>%
@@ -196,6 +179,7 @@ pltMonth = function(solName){
     # ggsave(paste("sol", panel, measurement, "sumWeek.pdf",sep=""), width = width, height = height, units = "cm")
     
     # return(smry)
+    return(sumDays)
 }
 
 
@@ -207,7 +191,8 @@ types = c('JA','LG')
 devices = c('_Bat', '_PV_')
 units = c('V', 'A', 'W')
 
-pltMonth("solD40JA_PV_W")
+checkitout = pltMonth("solD40JA_PV_W")
+print(checkitout)
 # pltMonth("solD40LG_PV_W")
 # pltMonth("solD90JA_PV_W")
 # pltMonth("solD90LG_PV_W")
