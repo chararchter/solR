a63e30c08bb75dc162539f9aaf25ba5c4c82b27f
chararchter <lentenis@inbox.lv>
2019-03-29 17:42:50 +0200

    Grouped functions in a more intuitive and comfy way to work

--- main.R
+++ main.R
@@ -17,19 +17,11 @@ height = 21.0
 
 setwd(paste(default, "code\\", sep=""))
 
-if(!exists("importData", mode="function")) source("importData.R")
-if(!exists("howMuchFiles", mode="function")) source("howMuchFiles.R")
-if(!exists("fixDatetime", mode="function")) source("fixDatetime.R")
-if(!exists("mergeData", mode="function")) source("mergeData.R")
-
-if(!exists("colNames", mode="function")) source("colNames.R")
-if(!exists("pltWeekStats", mode="function")) source("pltWeekStats.R")
-if(!exists("weekStats", mode="function")) source("weekStats.R")
-
-if(!exists("interpretSolPanel", mode="function")) source("interpretSolPanel.R")
-if(!exists("searchPattern", mode="function")) source("searchPattern.R")
-if(!exists("whichDevice", mode="function")) source("whichDevice.R")
-if(!exists("whichMeasure", mode="function")) source("whichMeasure.R")
+source("colNames.R")
+source("processData.R")
+source("parseCol.R")
+# source("week.R")
+source("integrate.R")
 
 #######################
 # import data
@@ -54,103 +46,6 @@ sharedTheme = theme_minimal()
 # Change the appearance of the main title
 sharedTheme = sharedTheme + theme(plot.title = element_text(size=18, face="bold",margin = margin(10, 0, 10, 0)))
 sharedAxis = xlab("Time")
-
-trapezoidArea = function(x, y){
-    # Calculate the area under the curve formed by connecting all points by a direct line
-    # (composite trapezoid rule). The curve must be given by vectors of xy-coordinates.
-    # Output - numeric value of area under the curve.
-    
-    t = 0
-    s = 0
-    
-    for (i in 1:(length(x)-1)){
-        deltx = x[i+1] - x[i]
-        yvid = (y[i] + y[i+1])/2
-        s[i] = deltx * yvid
-        t = t+s[i]
-    }
-    return(t)
-}
-
-integrateInterval = function(lowerLimit, delta_t, datTemp, solName){
-    # Defines lower and upper limits for trapezoidArea() integral.
-    # Output - data frame, where "time" - lower limit of interval;
-    # "sumkWh"  - value of area under the curve in that interval.
-    
-    solname = interpretSolPanel(solName)
-    var = produceStr(solname)
-    
-    timestamp = datTemp$timestamp
-    upperLimit = lowerLimit + delta_t
-
-    intrBig = (date(max(timestamp))-lowerLimit)
-    intrSmall = (upperLimit - lowerLimit)
-    itrTimes = floor(as.numeric(intrBig) / as.numeric(intrSmall))
-    
-    x <- as_datetime(itrTimes)
-    y <- numeric(itrTimes)
-    count = 0
-    i = 1
-    while (interval(lowerLimit, upperLimit) %within% interval(lowerLimit, date(max(timestamp)))) {
-        strtIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(lowerLimit)))[1]
-        endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(upperLimit)))[1]
-        datInt = datTemp[strtIndex:endIndex,]
-
-        t = trapezoidArea(datInt$timestamp, datInt$solVar)
-        kWh = t / 3600
-        count = count + kWh
-        # print(paste(interval(lowerLimit, upperLimit),"   ", "kWh =", format(kWh, digits = 2, nsmall=2), sep = " "))
-
-        intLength = int_length(interval(timestamp[strtIndex], timestamp[endIndex]))
-        x[i] <- date(timestamp[strtIndex])
-        y[i] <- kWh
-        
-        lowerLimit =  upperLimit
-        upperLimit = upperLimit + delta_t
-        i = i + 1
-    }
-    z = toString(var["panel"])
-    # sumInt = data.frame("time" = x, "sumkWh" = y)
-    sumInt = data.frame("day" = x, "solVar" = y)
-    colnames(sumInt)[2] <- toString(var["panel"])
-    print(sumInt)
-    return(sumInt)
-}
-
-
-
-sumMonth = function(datTemp, solName){
-    # Input - data frame where x - timestamp, y - solVar;
-    # Output - value of area under the curve in a month interval used for reference
-    # to check if it equals sum of kWh integrated by day in same month integral
-    solname = interpretSolPanel(solName)
-    var = produceStr(solname)
-    timestamp = datTemp$timestamp
-    
-    t = trapezoidArea(timestamp, datTemp$solVar)
-    kWhMonth = t /3600
-    print(solName)
-    print("Reference for a month")
-    print(paste(interval(date(min(timestamp)), date(max(timestamp))),"   ", "kWh =", format(kWhMonth, digits = 2, nsmall=2), sep = " "))
-    return(kWhMonth)
-}
-
-
-produceStr = function(solname){
-    # Input - solname after interpretSolPanel(solName)
-    # Output - combines useful strings of different solName parts to be used as titles, axis, and logical tests
-    
-    panel = paste(toString(solname["dir"]), toString(solname["degree"]), toString(solname["type"]), sep = "")
-    panelVerbose = paste("Solar panel ", panel, sep = "")
-    labelSolVar = paste(whichDevice(solname["device"]), whichMeasure(solname["unit"]), ", ", solname["unit"], sep = "")
-    measurement = paste("_", solname["device"], solname["unit"], sep = "")
-    
-    parameters = c(panel, panelVerbose, labelSolVar, measurement)
-    names(parameters) = c("panel", "panelVerbose", "labelSolVar", "measurement")
-    return(parameters)
-}
-
-
 
 pltMonth = function(solName){
     colIndex = which( colnames(datSol)==solName )
