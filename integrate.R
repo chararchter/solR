34380ae4bc840ce9a8811b26fb44cacd466a812b
chararchter <lentenis@inbox.lv>
2019-04-02 13:11:06 +0300

    Results start to seem plausible

--- integrate.R
+++ integrate.R
@@ -18,7 +18,7 @@ trapezoidArea = function(x, y){
 }
 
 integrateInterval = function(lowerLimit, delta_t, datTemp, solName){
-    # Defines lower and upper limits for trapezoidArea() integral.
+    # Defines lower and upper (atm end of the month) limits for trapezoidArea() integral.
     # Output - data frame, where "time" - lower limit of interval;
     # "sumkWh"  - value of area under the curve in that interval.   
     solname = interpretSolPanel(solName)
@@ -26,9 +26,9 @@ integrateInterval = function(lowerLimit, delta_t, datTemp, solName){
     
     timestamp = datTemp$timestamp
     upperLimit = lowerLimit + delta_t
-    intrBig = (date(max(timestamp))-lowerLimit)
-    intrSmall = (upperLimit - lowerLimit)
-    itrTimes = floor(as.numeric(intrBig) / as.numeric(intrSmall))
+    intrBig = (date(max(timestamp))-lowerLimit) # whole interval
+    intrSmall = (upperLimit - lowerLimit) #small interval
+    itrTimes = floor(as.numeric(intrBig) / as.numeric(intrSmall)) # how many small interval contains in big interval
     
     x <- as_datetime(itrTimes)
     y <- numeric(itrTimes)
@@ -55,8 +55,115 @@ integrateInterval = function(lowerLimit, delta_t, datTemp, solName){
     z = toString(var["panel"])
     sumInt = data.frame("day" = x, "solVar" = y)
     colnames(sumInt)[2] <- toString(var["panel"])
+    # print(sumInt)
+    return(sumInt)
+}
+
+integrateInterval2 = function(gLowerLimit, gUpperLimit, delta_t, datTemp, solName){
+    # Defines lower and upper limits for trapezoidArea() integral.
+    # Output - data frame, where "time" - lower limit of interval;
+    # "sumkWh"  - value of area under the curve in that interval.
+    # differs from integrateIntegral by introducing global and local limits. 
+    solname = interpretSolPanel(solName)
+    var = produceStr(solname)
+    
+    timestamp = datTemp$timestamp
+    locLowerLimit = gLowerLimit
+    locUpperLimit = gLowerLimit + delta_t
+    
+    diffBig = as.numeric(difftime(gUpperLimit, gLowerLimit, units="secs"))
+    diffSmall = lubridate::period_to_seconds(delta_t)
+    itrTimes = floor(diffBig / diffSmall) # how many small interval contains in big interval
+    
+    x <- as_datetime(itrTimes)
+    y <- numeric(itrTimes)
+    count = 0
+    i = 1
+    while (interval(locLowerLimit, locUpperLimit) %within% interval(gLowerLimit, gUpperLimit)) {
+        if (i == 1){
+            strtIndex = 1
+            endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locUpperLimit)))[1]
+        } else {
+            strtIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locLowerLimit)))[1] + 1
+            endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locUpperLimit)))[1]
+        }
+        datInt = datTemp[strtIndex:endIndex,]
+        
+        t = trapezoidArea(datInt$timestamp, datInt$solVar)
+        Wh = t / 3600
+        count = count + Wh
+        # print(paste(interval(lowerLimit, upperLimit),"   ", "Wh =", format(kWh, digits = 2, nsmall=2), sep = " "))
+        
+        intLength = int_length(interval(timestamp[strtIndex], timestamp[endIndex]))
+        x[i] <- date(timestamp[strtIndex])
+        y[i] <- Wh
+        
+        locLowerLimit =  locUpperLimit
+        locUpperLimit = locUpperLimit + delta_t
+        i = i + 1
+    }
+    z = toString(var["panel"])
+    sumInt = data.frame("day" = x, "solVar" = y)
+    colnames(sumInt)[2] <- toString(var["panel"])
     print(sumInt)
     return(sumInt)
+}
+
+
+
+integrateIntervalTest = function(gLowerLimit, gUpperLimit, delta_t, datTemp){
+    # Defines lower and upper (atm end of the month) limits for trapezoidArea() integral.
+    # Output - data frame, where "time" - lower limit of interval;
+    # "sumkWh"  - value of area under the curve in that interval.
+    # g means global
+    # loc means local
+
+    timestamp = datTemp$timestamp
+    locLowerLimit + gLowerLimit
+    locUpperLimit = gLowerLimit + delta_t
+    
+    diffBig = as.numeric(difftime(gUpperLimit, gLowerLimit, units="secs"))
+    diffSmall = lubridate::period_to_seconds(delta_t)
+    itrTimes = floor(diffBig / diffSmall) # how many small interval contains in big interval
+    
+    x <- as_datetime(itrTimes)
+    y <- numeric(itrTimes)
+    count = 0
+    i = 1
+    
+    for (i in 1:itrTimes){
+        
+    }
+    
+    # while (interval(locLowerLimit, locUpperLimit) %within% interval(gLowerLimit, gUpperLimit)) {
+    #     # find the index of the first datetime element in a vector where the date component matches with input date
+    #     # this wont work if start and end date are the same
+    #     # strtIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locLowerLimit)))[1]
+    #     # endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locUpperLimit)))[1]
+    # 
+    #     strtIndex = (which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locLowerLimit)))[1])
+    #     endIndex = which(date(as.POSIXct(timestamp)) == date(as.POSIXct(locUpperLimit)))[1]
+    #     
+    #     datInt = datTemp[strtIndex:endIndex,]
+    #     print(datInt)
+    #     t = trapezoidArea(datInt$timestamp, datInt$solVar)
+    #     # print(t)
+    #     Wh = t / 3600    # convert Ws to Wh
+    #     count = count + Wh
+    #     # print(paste(interval(lowerLimit, upperLimit),"   ", "kWh =", format(kWh, digits = 2, nsmall=2), sep = " "))
+    # 
+    #     intLength = int_length(interval(timestamp[strtIndex], timestamp[endIndex]))
+    #     x[i] <- date(timestamp[strtIndex])
+    #     y[i] <- Wh
+    # 
+    #     lowerLimit =  upperLimit
+    #     upperLimit = upperLimit + delta_t
+    #     i = i + 1
+    # }
+    # print(x)
+    # print(y)
+    # sumInt = data.frame("day" = x, "solVar" = y)
+    # return(sumInt)
 }
 
 sumMonth = function(datTemp, solName){
@@ -71,6 +178,6 @@ sumMonth = function(datTemp, solName){
     kWhMonth = t /3600
     print(solName)
     print("Reference for a month")
-    print(paste(interval(date(min(timestamp)), date(max(timestamp))),"   ", "kWh =", format(kWhMonth, digits = 2, nsmall=2), sep = " "))
+    print(paste(interval(date(min(timestamp)), date(max(timestamp))),"   ", "Wh =", format(kWhMonth, digits = 2, nsmall=2), sep = " "))
     return(kWhMonth)
 }
--- tests/testIntegrate.R
+++ tests/testIntegrate.R
@@ -2,33 +2,46 @@ default = "F:\\Users\\Janis\\VIKA\\solR\\code\\"
 setwd(default)
 source("integrate.R")
 
-# test1
-x = seq(1,7)
-y = c(0,0,2,2,2,0,0)
-plot(x,y)
+############################
+# trapezoidArea
+############################
 
-area = trapezoidArea(x,y)
-print(area)
+# test1: now doesnt work on floats, cuz deltx calculates difftime(x[i+1], + x[i], unit="secs") now and needs POSIXct
+# x = seq(1,7)
+# y = c(0,0,2,2,2,0,0)
+# plot(x,y)
+# 
+# area = trapezoidArea(x,y)
+# print(area)
 
 # test2 w POSIXt values & min interval
 timestamp <- seq.POSIXt(from=as.POSIXct("2019-01-01 00:00:00"),
-                        to=as.POSIXct("2019-01-01 01:30:00"), by= '30 min')
+                        to=as.POSIXct("2019-01-02 02:00:00"), by= '30 min')
 solVar = seq(from = 1, to = 10, length.out = length(timestamp))
 plot(timestamp,solVar)
+df = data.frame(timestamp, solVar)
 
-area2 = trapezoidAreaTest(timestamp,solVar)
+area2 = trapezoidArea(timestamp,solVar)
 print(paste("Ws =   ", area2, sep=""))
 print(paste("Wh =   ", area2/(60*60), sep=""))
 
 # test3 w POSIXt values & seconds interval
-timestamp <- seq.POSIXt(from=as.POSIXct("2019-01-01 00:00:00"),
-                        to=as.POSIXct("2019-01-01 00:04:00"), by= '30 sec')
-solVar = seq(from = 1, to = 10, length.out = length(timestamp))
-plot(timestamp,solVar)
-area3 = trapezoidAreaTest(timestamp,solVar)
-
-print(paste("Ws =   ", area3, sep=""))
-print(paste("Wh =   ", area3/(60*60), sep=""))
+# timestamp <- seq.POSIXt(from=as.POSIXct("2019-01-01 00:00:00"),
+#                         to=as.POSIXct("2019-01-01 00:06:00"), by= '30 sec')
+# solVar = seq(from = 1, to = 10, length.out = length(timestamp))
+# df = data.frame(timestamp, solVar)
+# plot(timestamp,solVar)
+# area3 = trapezoidArea(timestamp,solVar)
+# print(paste("Ws =   ", area3, sep=""))
+# print(paste("Wh =   ", area3/(60*60), sep=""))
 
 # test how difftime works
-print(as.numeric(difftime(as.POSIXct("2019-01-01 00:00:00"), + as.POSIXct("2019-01-01 00:02:00"), unit="secs")))
+# print(as.numeric(difftime(as.POSIXct("2019-01-01 00:00:00"), + as.POSIXct("2019-01-01 00:02:00"), unit="secs")))
+
+
+############################
+# integrateInterval
+############################
+
+
+testMin = integrateIntervalTest(min(df$timestamp), max(df$timestamp), minutes(60), df)
