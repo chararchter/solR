93bc5515d51541fadc49fad14bc20577bd6d8ba4
chararchter <lentenis@inbox.lv>
2019-05-19 16:22:21 +0300

    Draws most plots

--- plots.R
+++ plots.R
@@ -50,10 +50,6 @@ sumDay.tidy = tidy(sumDay)
 sumWeek.tidy = tidy(sumWeek)
 sumMonth.tidy = tidy(sumMonth)
 
-sumMonth %>%
-    gather(key, Wh, -timestamp, -type) %>%
-    separate(key, c("DirDegree", "Type"), "\\.")
-
 # calculate differences between two values
 dif = numeric(nrow(sumMin.tidy))
 dif[1] = 0
@@ -66,14 +62,11 @@ sumDaym2 = whToWhm2(sumDay.tidy)
 sumWeekm2 = whToWhm2(sumWeek.tidy)
 sumMonthm2 = whToWhm2(sumMonth.tidy)
 
-
 datMin = read.csv("2019-03_whMins_atstarpes.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
 datMin.tidy = tidy(datMin)
 datMin.tidy = datMin.tidy  %>% separate(timestamp, c("date", "time"), sep=" ")
 datMin.tidy$date = ymd(datMin.tidy$date)
 datMin.tidy$time = hms(datMin.tidy$time)
-
-
 
 ######################################
 # plot
@@ -85,11 +78,24 @@ names(col13) <- c("D", "A", "R")
 colD = c("#00B788", "#036C9B", "#003554")
 names(colD) <- c("13", "40", "90")
 
+colPan = c("#00B788", "#03C133", "#00612E", "#036C9B", "#003554")
+names(colPan) <- c("D.13", "A.13", "R.13", "D.40", "D.90")
+scaleColPan <- scale_colour_manual(name = "DirDeg", values=colPan)
+
 scaleCol13 <- scale_colour_manual(name = "Dir", values=col13)
 scaleColD <- scale_colour_manual(name = "Degree", values=colD)
 
 scaleFill13 <- scale_fill_manual(name = "Dir", values=col13)
 scaleFillD <- scale_fill_manual(name = "Degree", values=colD)
+
+minm2.13 = sumMinm2 %>% filter(Degree == "13")
+minm2.D = sumMinm2 %>% filter(Dir == "D")
+hm2.13 = sumHourm2 %>% filter(Degree == "13")
+hm2.D = sumHourm2 %>% filter(Dir == "D")
+daym2.13 = sumDaym2 %>% filter(Degree == "13")
+daym2.D = sumDaym2 %>% filter(Dir == "D")
+minDif.13 = sumMin.dif %>% filter(Degree == "13")
+minDif.D = sumMin.dif %>% filter(Dir == "D")
 
 min.13 = sumMin.tidy %>% filter(Degree == "13")
 min.D = sumMin.tidy %>% filter(Dir == "D")
@@ -97,6 +103,7 @@ h.13 = sumHour.tidy %>% filter(Degree == "13")
 h.D = sumHour.tidy %>% filter(Dir == "D")
 day.13 = sumDay.tidy %>% filter(Degree == "13")
 day.D = sumDay.tidy %>% filter(Dir == "D")
+
 
 pnt = geom_point(alpha = 0.8)
 pnt2 = geom_point(alpha = 0.8, shape = 1)
@@ -110,6 +117,15 @@ labDeg = labs(x = "t, d", y = "E, kWh", shape = "Tips", col = "Lenkis")
 labDir = labs(x = "t, d", y = "E, kWh", shape = "Tips", col = "Virziens")
 labDeg2 = labs(x = "t, h", y = "E, Wh", shape = "Tips", col = "Lenkis")
 labDir2 = labs(x = "t, h", y = "E, Wh", shape = "Tips", col = "Virziens")
+
+labDeg3 = labs(x = "t, d", y = "delta E, Wh", shape = "Tips", col = "Lenkis")
+labDir3 = labs(x = "t, d", y = "delta E, Wh", shape = "Tips", col = "Virziens")
+
+labDegm2 = labs(x = "t, d", y = "E, kWh/m2", shape = "Tips", col = "Lenkis")
+labDirm2 = labs(x = "t, d", y = "E, kWh/m2", shape = "Tips", col = "Virziens")
+labDeg2m2 = labs(x = "t, h", y = "E, Wh/m2", shape = "Tips", col = "Lenkis")
+labDir2m2 = labs(x = "t, h", y = "E, Wh/m2", shape = "Tips", col = "Virziens")
+
 labDegType = labs(x = "Laiks", y = "E, kWh", col = "Leņķis")
 labDirType = labs(x = "Laiks", y = "E, kWh", col = "Virziens")
 labBase = labs(x = "Laiks", y = "E, kWh")
@@ -117,12 +133,14 @@ labBase = labs(x = "Laiks", y = "E, kWh")
 gridDir = facet_grid(. ~ Dir)
 gridDeg = facet_grid(. ~ Degree)
 shp = scale_shape_manual(values=c(1, 2))
+shp3 = scale_shape_manual(values=c(".", "."))
 
 intrval1 = c(min(sumHour$timestamp) + days(19) + hours(7), min(sumHour$timestamp) + days(20) - hours(7))
 cord = coord_cartesian(xlim = intrval1)
 
 
-deg = function(data){
+deg = function(data, param){
+    if (param == "Wh"){
     d = ggplot(data, aes(x = timestamp, y = Wh/1000, col = Degree, shape = Type)) +
         pnt + axTime + labDeg + shp + ggtitle("Direction: D") + scaleColD
     d2 = d + facet_grid(Type ~ Degree)  + awoLegend
@@ -132,9 +150,21 @@ deg = function(data){
         axTime + labDeg + shp + ggtitle("Direction: D") + scaleFillD
     d2 = d + facet_grid(Type ~ Degree)  + awoLegend
     export_pdf(d2, "Degbar")
+    } else {
+        d = ggplot(data, aes(x = timestamp, y = Whm2/1000, col = Degree, shape = Type)) +
+            pnt + axTime + labDegm2 + shp + ggtitle("Direction: D") + scaleColD
+        d2 = d + facet_grid(Type ~ Degree)  + awoLegend
+        export_pdf(d2, "Degm2")
+        d = ggplot(data, aes(x = timestamp, y = Whm2/1000, fill = Degree, shape = Type)) +
+            geom_bar(stat = "identity", position=position_dodge()) +
+            axTime + labDegm2 + shp + ggtitle("Direction: D") + scaleFillD
+        d2 = d + facet_grid(Type ~ Degree)  + awoLegend
+        export_pdf(d2, "Degbarm2")
+        }
 }
 
-dir = function(data){
+dir = function(data, param){
+    if (param == "Wh"){
     p = ggplot(data, aes(x = timestamp, y = Wh/1000, col = Dir, shape = Type)) +
         pnt + axTime + labDir + shp + ggtitle("Degree: 13") + scaleCol13
     p2 = p + facet_grid(Type ~ Dir) + awoLegend
@@ -144,33 +174,80 @@ dir = function(data){
         axTime + labDir + shp + ggtitle("Degree: 13") + scaleFill13
     p2 = p + facet_grid(Type ~ Dir) + awoLegend
     export_pdf(p2, "Dirbar")
+    }
+    else{
+        p = ggplot(data, aes(x = timestamp, y = Whm2/1000, col = Dir, shape = Type)) +
+            pnt + axTime + labDirm2 + shp + ggtitle("Degree: 13") + scaleCol13
+        p2 = p + facet_grid(Type ~ Dir) + awoLegend
+        export_pdf(p2, "Dirm2")
+        p = ggplot(data, aes(x = timestamp, y = Whm2/1000, fill = Dir, shape = Type)) +
+            geom_bar(stat = "identity", position=position_dodge()) + 
+            axTime + labDirm2 + shp + ggtitle("Degree: 13") + scaleFill13
+        p2 = p + facet_grid(Type ~ Dir) + awoLegend
+        export_pdf(p2, "Dirbarm2")
+    }
 }
 
 
-dayDeg  = function(data){
+dayDeg  = function(data, param){
+    if (param == "Wh"){
     d = ggplot(data, aes(x = timestamp, y = Wh, col = Degree, shape = Type)) +
         geom_line() + axTime2 + labDeg2 + shp + ggtitle("Direction: D") + scaleColD + cord
     d2 = d + facet_grid(Type ~ Degree)  + awoLegend
     export_pdf(d2, "Deg_l")
+    }
+    else{
+        d = ggplot(data, aes(x = timestamp, y = Whm2, col = Degree, shape = Type)) +
+            geom_line() + axTime2 + labDeg2m2 + shp + ggtitle("Direction: D") + scaleColD + cord
+        d2 = d + facet_grid(Type ~ Degree)  + awoLegend
+        export_pdf(d2, "Deg_lm2")
+    }
 }
 
-dayDir  = function(data){
+dayDir  = function(data, param){
+    if (param == "Wh"){
     p = ggplot(data, aes(x = timestamp, y = Wh, col = Dir, shape = Type)) +
         geom_line() + axTime2 + labDir2 + shp + ggtitle("Degree: 13") + scaleCol13 + cord
     p2 = p + facet_grid(Type ~ Dir) + awoLegend
     export_pdf(p2, "Dir_l")
+    }
+    else{
+        p = ggplot(data, aes(x = timestamp, y = Whm2, col = Dir, shape = Type)) +
+            geom_line() + axTime2 + labDir2m2 + shp + ggtitle("Degree: 13") + scaleCol13 + cord
+        p2 = p + facet_grid(Type ~ Dir) + awoLegend
+        export_pdf(p2, "Dir_lm2")
+    }
+}
+
+difDir = function(data){
+    p = ggplot(data, aes(x = timestamp, y = Wh, col = Dir, shape = Type)) +
+        geom_point(alpha = 0.3) + axTime + labDir3 + shp3 + ggtitle("Degree: 13") + scaleCol13
+    p2 = p + facet_grid(Type ~ Dir) + awoLegend
+    export_pdf(p2, "difDir")
+}
+
+difDeg = function(data){
+    p = ggplot(data, aes(x = timestamp, y = Wh, col = Degree, shape = Type)) +
+        geom_point(alpha = 0.3) + axTime + labDeg3 + shp3 + ggtitle("Direction: D") + scaleColD
+    p2 = p + facet_grid(Type ~ Degree) + awoLegend
+    export_pdf(p2, "difDeg")
 }
 
 month  = function(data){
-    p = ggplot(data, aes(x = timestamp, y = Wh, col = Dir, shape = Type)) +
-        geom_line() + axTime2 + labDir2 + shp + ggtitle("Degree: 13") + scaleCol13 + cord
-    p2 = p + facet_grid(Type ~ Dir) + awoLegend
-    export_pdf(p2, "Dir_l")
+    # not working cuz x = timestamp is invalid (every panel occupys same spot and it looks like gray rectangle)
+    p = ggplot(data, aes(x = timestamp, y = Wh/1000)) + geom_bar(stat = "identity", position=position_dodge()) + ggtitle("Marts")
+    export_pdf(p, "mar_bar")
 }
 
 
-deg(day.D)
-dir(day.13)
+deg(day.D, "Wh")
+dir(day.13, "Wh")
+dayDeg(min.D, "Wh")
+dayDir(min.13, "Wh")
+difDeg(minDif.D)
+difDir(minDif.13)
 
-dayDeg(min.D)
-dayDir(min.13)
+# deg(daym2.D, "Whm2")
+# dir(daym2.13, "Whm2")
+# dayDeg(minm2.D, "Whm2")
+# dayDir(minm2.13, "Whm2")
