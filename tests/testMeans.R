c1f2a1b80b18f1e5f79a50f809221bc1baf3f932
chararchter <lentenis@inbox.lv>
2019-05-21 14:32:22 +0300

    Included gradient and colorbox guide in a plot

--- tests/testMeans.R
+++ tests/testMeans.R
@@ -19,11 +19,10 @@ tidy = function(data){
 }
 
 datMin = read.csv("2019-03_whMins_atstarpes.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
-# datMin = read.csv("sumHour_atstarpes.csv", header = TRUE, sep = ",", stringsAsFactors = FALSE)
+datMin$timestamp = as.POSIXct(datMin$timestamp)
 datMin.tidy = tidy(datMin)
 datMin.tidy = datMin.tidy  %>% separate(timestamp, c("date", "time"), sep=" ")
 
-# dummyDate = rep(datMin.tidy$date[1], each=nrow(datMin.tidy))
 dummyDate = datMin.tidy$date[1]
 datMin.tidy$date = ymd(datMin.tidy$date)
 datMin.tidy$date = format(datMin.tidy$date, format="%y%m-%d")
@@ -49,32 +48,19 @@ shp = scale_shape_manual(values=c(1, 2))
 
 data.13 = datMin.tidier %>% filter(Degree == "13")
 data.D = datMin.tidier %>% filter(Dir == "D")
-# experiment$Grouping <- as.factor(experiment$Group) 
 
 # d = ggplot(data.D, aes(x = fakeTime, y = Wh, col = Degree, linetype = Type)) +
 #     geom_line(alpha = 0.5) + axTime + labDeg2 + shp + ggtitle("Direction: D") + scaleColD
 # d2 = d + facet_grid(Type ~ Degree)
 # export_pdf(d2, "test")
 
-# testScale = scale_color_manual(values=["blue", "purple", "orange"])
-#    scale_colour_gradient(low = "#132B43", high = "#56B1F7",
-#                      space = "Lab", na.value = "grey50", guide = "colourbar",
-#                      aesthetics = "colour")
-
-
 datMin.tidier$Date = as.numeric(datMin.tidier$Date)
 datMin.tidier$Degree = as.numeric(datMin.tidier$Degree)
 write.csv(datMin.tidier, file = "killme.csv", row.names=FALSE)
 
-# geom_line(data=data.frame(spline(data.test, n=500)))
+data.test = datMin.tidier %>% filter(Dir == "D" & Degree == 40 & Type == "JA" )
+data.test = data.test %>% filter(Date %% 5 == 0)
 
-data.test = datMin.tidier %>% filter(Dir == "D" & Degree == 40 & Type == "JA" & Date < 3)
-data.test <- transform(data.test,Date=factor(Date,levels=unique(Date)))
-write.csv(data.test, file = "killme666.csv", row.names=FALSE)
-data.spline = data.frame(fakeTime = data.test$fakeTime, Wh = data.test$Wh)
-
-d = ggplot(data.test, aes(x = fakeTime, y = Wh, col = Date)) +
-    geom_line(alpha = 0.5) + scale_color_manual(values=c("blue", "purple"))
-    # geom_line(data=data.frame(spline(data.spline, n=300)))
-# d2 = d + facet_grid(Type ~ Degree)
-export_pdf(d, "test1")
+d = ggplot(data.test, aes(x = fakeTime, y = Wh, group = as.numeric(Date))) + geom_line(aes(col = Date)) +
+scale_colour_gradient2()
+export_pdf(d, "test6")
